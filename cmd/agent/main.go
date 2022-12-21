56c8321f81df1f0632d3dae185f88e8657775475
artems723 <artem723@gmail.com>
2022-12-21 17:51:38 +0300

    added url's

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"net/http"
 	"runtime"
 	"time"
 )
@@ -13,7 +12,6 @@ type counter int64
 
 type monitor struct {
 	rtm           runtime.MemStats
-	client        http.Client
 	Alloc         gauge
 	BuckHashSys   gauge
 	Frees         gauge
@@ -83,12 +81,50 @@ func (m *monitor) updateMonitor() {
 
 func (m *monitor) sendData() {
 
-	client := &http.Client{}
-	client.Timeout = time.Second * 1
+	endpoint := "127.0.0.1"
+	port := 8080
 
-	//resp, err := client.Post("http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>", "text/plain", nil)
+	urlList := []string{
+		fmt.Sprintf("http://%s:%d/update/gauge/Alloc/%f", endpoint, port, m.Alloc),
+		fmt.Sprintf("http://%s:%d/update/gauge/BuckHashSys/%f", endpoint, port, m.BuckHashSys),
+		fmt.Sprintf("http://%s:%d/update/gauge/Frees/%f", endpoint, port, m.Frees),
+		fmt.Sprintf("http://%s:%d/update/gauge/GCCPUFraction/%f", endpoint, port, m.GCCPUFraction),
+		fmt.Sprintf("http://%s:%d/update/gauge/GCSys/%f", endpoint, port, m.GCSys),
+		fmt.Sprintf("http://%s:%d/update/gauge/HeapAlloc/%f", endpoint, port, m.HeapAlloc),
+		fmt.Sprintf("http://%s:%d/update/gauge/HeapIdle/%f", endpoint, port, m.HeapIdle),
+		fmt.Sprintf("http://%s:%d/update/gauge/HeapInuse/%f", endpoint, port, m.HeapInuse),
+		fmt.Sprintf("http://%s:%d/update/gauge/HeapObjects/%f", endpoint, port, m.HeapObjects),
+		fmt.Sprintf("http://%s:%d/update/gauge/HeapReleased/%f", endpoint, port, m.HeapReleased),
+		fmt.Sprintf("http://%s:%d/update/gauge/HeapSys/%f", endpoint, port, m.HeapSys),
+		fmt.Sprintf("http://%s:%d/update/gauge/LastGC/%f", endpoint, port, m.LastGC),
+		fmt.Sprintf("http://%s:%d/update/gauge/Lookups/%f", endpoint, port, m.Lookups),
+		fmt.Sprintf("http://%s:%d/update/gauge/MCacheInuse/%f", endpoint, port, m.MCacheInuse),
+		fmt.Sprintf("http://%s:%d/update/gauge/MCacheSys/%f", endpoint, port, m.MCacheSys),
+		fmt.Sprintf("http://%s:%d/update/gauge/MSpanInuse/%f", endpoint, port, m.MSpanInuse),
+		fmt.Sprintf("http://%s:%d/update/gauge/MSpanSys/%f", endpoint, port, m.MSpanSys),
+		fmt.Sprintf("http://%s:%d/update/gauge/Mallocs/%f", endpoint, port, m.Mallocs),
+		fmt.Sprintf("http://%s:%d/update/gauge/NextGC/%f", endpoint, port, m.NextGC),
+		fmt.Sprintf("http://%s:%d/update/gauge/NumForcedGC/%f", endpoint, port, m.NumForcedGC),
+		fmt.Sprintf("http://%s:%d/update/gauge/NumGC/%f", endpoint, port, m.NumGC),
+		fmt.Sprintf("http://%s:%d/update/gauge/OtherSys/%f", endpoint, port, m.OtherSys),
+		fmt.Sprintf("http://%s:%d/update/gauge/PauseTotalNs/%f", endpoint, port, m.PauseTotalNs),
+		fmt.Sprintf("http://%s:%d/update/gauge/StackInuse/%f", endpoint, port, m.StackInuse),
+		fmt.Sprintf("http://%s:%d/update/gauge/StackSys/%f", endpoint, port, m.StackSys),
+		fmt.Sprintf("http://%s:%d/update/gauge/Sys/%f", endpoint, port, m.Sys),
+		fmt.Sprintf("http://%s:%d/update/gauge/TotalAlloc/%f", endpoint, port, m.TotalAlloc),
+		fmt.Sprintf("http://%s:%d/update/counter/PollCount/%d", endpoint, port, m.PollCount),
+		fmt.Sprintf("http://%s:%d/update/gauge/RandomValue/%f", endpoint, port, m.RandomValue),
+	}
 
-	fmt.Println("Sending data")
+	for _, url := range urlList {
+		//_, err := http.Post(url, "text/plain", nil)
+		//if err != nil {
+		//	// handle error
+		//}
+
+		fmt.Printf("Sending data to %s\n", url)
+	}
+
 	m.PollCount = 0
 }
 
