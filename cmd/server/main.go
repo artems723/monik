096b521817a89c41a5fa0b50e09ec5a2a442811c
artems723 <artem723@gmail.com>
2022-12-22 15:52:22 +0300

    added http server for client testing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Got request. Method=%s. Path=%s\n", r.Method, r.URL.Path)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	path := r.URL.Path
+	fmt.Printf("%s\n", path)
+}
+
+func main() {
+	http.HandleFunc("/update/", uploadHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
