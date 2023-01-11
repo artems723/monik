86a0e4ee7aebeda6d708fab7b41e1e5374bb83f5
artems723 <artem723@gmail.com>
2023-01-11 11:19:21 +0300

    got rid of 'else'

--- internal/server/handler/mainPage.go
+++ internal/server/handler/mainPage.go
@@ -13,14 +13,16 @@ func (h *Handler) mainPage(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Got main page request. Method=%s Path: %s agentID: %s \n", r.Method, r.URL.Path, agentID)
 
 	allMetrics, ok := h.s.GetAllMetrics(agentID)
-	if ok {
-		b := new(bytes.Buffer)
-		for key, value := range allMetrics {
-			fmt.Fprintf(b, "%s=\"%s\"\n", key, value)
-		}
-		w.Write(b.Bytes())
-		w.WriteHeader(http.StatusOK)
-	} else {
+	if !ok {
 		w.WriteHeader(http.StatusNotFound)
+		return
 	}
+
+	b := new(bytes.Buffer)
+	for key, value := range allMetrics {
+		fmt.Fprintf(b, "%s=\"%s\"\n", key, value)
+	}
+	w.Write(b.Bytes())
+	w.WriteHeader(http.StatusOK)
+
 }
