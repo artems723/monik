97b998546ae7257193280e19c66a90e48a3521fb
artem723 <artem723@gmail.com>
2023-01-30 22:59:15 +0300

    add getAll and writeAll metric impl for postgres

--- internal/server/storage/postgresStorage.go
+++ internal/server/storage/postgresStorage.go
@@ -2,7 +2,9 @@ package storage
 
 import (
 	"context"
+	"database/sql"
 	"embed"
+	"errors"
 	"github.com/artems723/monik/internal/server/domain"
 	"github.com/jmoiron/sqlx"
 	"log"
@@ -39,7 +41,10 @@ func NewPostgresStorage(databaseDSN string) (*PostgresStorage, error) {
 func (p *PostgresStorage) GetMetric(ctx context.Context, metricName string) (*domain.Metric, error) {
 	var m domain.Metric
 	err := p.db.Get(&m, "SELECT name,type,delta,value FROM metrics WHERE name=$1", metricName)
-	if err != nil {
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, err
+	}
+	if err == sql.ErrNoRows {
 		return nil, ErrNotFound
 	}
 	return &m, nil
@@ -51,17 +56,35 @@ func (p *PostgresStorage) WriteMetric(ctx context.Context, metric *domain.Metric
 	if err != nil {
 		return err
 	}
-	tx.Commit()
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (p *PostgresStorage) GetAllMetrics(ctx context.Context) (*domain.Metrics, error) {
-	// TODO
-	return nil, nil
+	var m []*domain.Metric
+	err := p.db.Select(&m, "SELECT name,type,delta,value FROM metrics")
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return nil, err
+	}
+	if err == sql.ErrNoRows {
+		return nil, ErrNotFound
+	}
+	return &domain.Metrics{Metrics: m}, nil
 }
 
 func (p *PostgresStorage) WriteAllMetrics(ctx context.Context, metrics *domain.Metrics) error {
-	// TODO
+	tx := p.db.MustBegin()
+	_, err := tx.NamedExec("INSERT INTO metrics (name, type, delta, value) VALUES (:name, :type, :delta, :value) ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name, type = EXCLUDED.type, delta = EXCLUDED.delta, value = EXCLUDED.value", &metrics.Metrics)
+	if err != nil {
+		return err
+	}
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
