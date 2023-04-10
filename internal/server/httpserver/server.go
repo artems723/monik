baf8823cbd6cadb33d5b79f33c97e8a2aefa5176
artems723 <artem723@gmail.com>
2023-04-10 12:35:25 +0300

    add comment for server write timeout

--- internal/server/httpserver/server.go
+++ internal/server/httpserver/server.go
@@ -22,8 +22,9 @@ func (s *Server) Run(serverAddr string, r *chi.Mux) error {
 		Addr:           serverAddr,
 		MaxHeaderBytes: 1 << 20, // 1MB
 		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   31 * time.Second,
-		Handler:        r,
+		// Set write timeout >30 sec. It is needed to run stress test for 30 sec using Vegeta https://github.com/tsenart/vegeta
+		WriteTimeout: 31 * time.Second,
+		Handler:      r,
 	}
 	return s.httpServer.ListenAndServe()
 }
