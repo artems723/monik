e527f774751410e2584307ba1b9f65b67d4be126
artem723 <artem723@gmail.com>
2023-04-18 23:09:00 +0300

    add content-type

--- internal/server/handler/update.go
+++ internal/server/handler/update.go
@@ -133,6 +133,6 @@ func (h *Handler) updateMetricsJSON(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
